f31db535b04c7c7e029f388633acd665c17fb933
John <christophermoreno.jm2@gmail.com>
2022-02-23 08:44:36 +0800

    added features

--- script.js
+++ script.js
@@ -15,23 +15,73 @@ const minus = document.getElementById('-');
 const times = document.getElementById('*');
 const divide = document.getElementById('÷');
 const equals = document.getElementById('=');
-const displayValuesBot = [nine, eight, seven, six, five, four, three, two, one, zero];
-const displayValuesTop = [plus, minus, times, divide];
 
-changeDisplayValue(displayValuesBot, displayValuesTop)
+const numbers = [nine, eight, seven, six, five, four, three, two, one, zero];
+const operators = [plus, minus, times, divide];
+let chosenNumbers = [];
+let chosenNumbers2=[];
+let toCalculate = [];
+let calculate = [];
+let answer;
 
-function changeDisplayValue(displayValuesBot, displayValuesTop){
-    for (let i=0; i<displayValuesBot.length; i++) {
-        displayValuesBot[i].addEventListener('click',() => {
-            botValue.textContent+=displayValuesBot[i].id
+changeDisplayValue(numbers, operators)
+operate()
+
+function changeDisplayValue(numbers, operators){
+    for (let i=0; i<numbers.length; i++) {
+        numbers[i].addEventListener('click',() => {
+            chosenNumbers += numbers[i].id
+            botValue.textContent = chosenNumbers;  
         });
     } 
     
-    for (let i=0; i<displayValuesTop.length; i++)
-    displayValuesTop[i].addEventListener('click',() => {
-        topValue.textContent=botValue.textContent + displayValuesTop[i].id
+    for (let i=0; i<operators.length; i++)
+    operators[i].addEventListener('click',() => {
+            toCalculate.push(chosenNumbers);
+            chosenNumbers = []
+        calculate.push(operators[i].id);
+        chosenNumbers2=toCalculate[0] + operators[i].id
+        topValue.textContent=chosenNumbers2;
+
     });
 }
+
+function operate(){
+    for (let i=0; i<operators.length; i++)
+    operators[i].addEventListener('click',() => {
+    
+        if (toCalculate.length == 2){
+            switch(calculate[0]){
+                case "+":
+                   answer = add(Number(toCalculate[0]),Number(toCalculate[1]))
+                   toCalculate = []
+                   toCalculate[0] = answer;
+                   calculate.splice(0,1)
+                    break;
+                case "-":
+                   answer = subtract(toCalculate[0], toCalculate[1])
+                   toCalculate = []
+                   toCalculate[0] = answer;
+                   calculate.splice(0,1)
+                    break;
+                case "*":
+                    answer = multiply(toCalculate[0], toCalculate[1])
+                    toCalculate = []
+                    toCalculate[0] = answer;
+                    calculate.splice(0,1)
+                    break;
+                case "÷":
+                    answer = division(toCalculate[0], toCalculate[1])
+                    toCalculate = []
+                    toCalculate[0] = answer;
+                    calculate.splice(0,1)
+                    break;
+        }
+        botValue.textContent = answer
+        topValue.textContent = answer + operators[i].id
+        }
+    });
+};
 
 function add(a,b){
     return a+b
@@ -48,14 +98,3 @@ function multiply(a,b){
 function division(a,b){
     return a/b
 };
-
-let  array = [];
-// const result = operate(array){
-//     array.reduce(a,b){(a,b) =>  }
-// }
-
-function operate(array){
-    const result = array.reduce(add)
-    return result;
-};
-    
